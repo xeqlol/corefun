bb473d457a59327da6879e4372f43e2cf52a73d5
Dmitry Nemkov <xeqlol@gmail.com>
2017-09-04 21:08:47 +0500

    complete step 8

--- CoreFun/Migrations/20170904154352_RowVersion.Designer.cs
+++ CoreFun/Migrations/20170904154352_RowVersion.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace CoreFun.Migrations
 {
     [DbContext(typeof(SchoolContext))]
-    partial class SchoolContextModelSnapshot : ModelSnapshot
+    [Migration("20170904154352_RowVersion")]
+    partial class RowVersion
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
